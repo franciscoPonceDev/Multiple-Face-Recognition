a56c56d83f5c4c59c01dde3868396a3818be9375
Francisco G P Ponce <francisco.gp.ponce@gmail.com>
2021-02-26 21:28:51 -0300

    deploy

--- src/App.js
+++ src/App.js
@@ -91,7 +91,7 @@ onButtonSubmit = () => {
   .then(response => {
     console.log('hi', response)
     if (response) {
-      fetch('http://localhost:3000/image', {
+      fetch('https://immense-bastion-19449.herokuapp.com/image', {
         method: 'put',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component {
       }
     
       onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://immense-bastion-19449.herokuapp.com/signin', {
           method: 'post',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
